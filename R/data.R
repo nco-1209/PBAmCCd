511cf0968ad6995bc3fe03b688d667341c0f957d
nco-1209 <okaemenneka@gmail.com>
2022-08-30 15:08:32 -0400

    data

--- R/data.R
+++ R/data.R
@@ -1,25 +1,3 @@
-#' MLE Data
-#'
-#' Dataset of maximum likelihood estimated data used for simulating single-cell mRNA data.
-#' 
-#'
-#' @docType data
-#'
-#' @usage data(mle.single.cell)
-#'
-#' @format Gives mle estimates of \code{mu} and \code{Sigma}, as well as a single-cell
-#' dataset of 96 samples and 100 genes.  
-#'
-#' @keywords datasets
-#'
-#' @examples
-#' data(mle.single.cell)
-#' mu <- mle$est.min$mu
-#' Sigma <- mle$est.min$Sigma
-#' 
-#' 
-"mle.single.cell"
-
 #' dat.ss
 #'
 #' The single-cell mRNA dataset of 96 samples (rows), and 100 genes (columns). 
